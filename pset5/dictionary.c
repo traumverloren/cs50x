f1e26965154c2fa24b0ae8589fbf9c46ba4b3746
Stephanie Nemeth <traumverloren@gmail.com>
2016-10-18 16:41:05 +0200

    dictionary loading is working

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -8,8 +8,37 @@
  */
 
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "dictionary.h"
+
+// trie structure of node:
+// each child node has 27 spots due to 26 letters + apostrophe
+typedef struct node
+{
+    bool isWord;
+    struct node* children[27];
+}
+node;
+
+// set root node
+node* root;
+
+// initialize wordCount
+unsigned int wordCount;
+
+int findIndex(const char c)
+{
+    if (c == '\'')
+    {
+        return 26;
+    }
+    else
+    {
+        return (c % 'a');
+    }
+}
 
 /**
  * Returns true if word is in dictionary else false.
@@ -25,8 +54,49 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    // allocate memory for the root node
+    root = calloc(1, sizeof(node));
+
+    // initially set the node pointer to the root
+    node* pointer = root;
+    
+    // open dictionary file
+    FILE* fp = fopen(dictionary, "r");
+
+    // for every dictionary word, iterate thru the trie
+    for (char c = tolower(fgetc(fp)); c != EOF; c = fgetc(fp))
+    {
+        // at end of word, set isWord to true and reset pointer to root, and add to word count
+        if (c == '\n')
+        {
+            pointer->isWord = true;
+            pointer = root;
+            wordCount++;
+        }
+        
+        // word is still in progress: find index of each letter, build the trie
+        else 
+        {
+            // assign index for c
+            int index = findIndex(c);
+
+            // check to see if that node exists, if not, allocate memory and create it.
+            if (pointer->children[index] == NULL)
+            {
+                pointer->children[index] = calloc(1, sizeof(node));
+            }
+            
+            // set pointer to the next node.
+            pointer = pointer->children[index];
+                
+        }
+    }
+    
+    printf("%i", wordCount);
+    
+    fclose(fp);
+    
+    return true;
 }
 
 /**
@@ -34,8 +104,7 @@ bool load(const char* dictionary)
  */
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return wordCount;
 }
 
 /**
