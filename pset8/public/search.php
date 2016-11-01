9ee71297f9003a8ef923c301389e5031a428aeef
Stephanie Nemeth <traumverloren@gmail.com>
2016-11-01 21:12:19 +0100

    write search php method, fine tune sql fulltext query search

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -2,10 +2,42 @@
 
     require(__DIR__ . "/../includes/config.php");
 
+    $query = preg_split("/[\s,+]+/", $_GET["geo"]);
+    
+    if (is_numeric($query[0]))
+    {
+        $search_string = $query[0];
+    }
+    else
+    {
+        $query = preg_filter('/^/', '+', $query);
+        $search_string = implode(" ", $query);
+    }
+    
     // numerically indexed array of places
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
+    $rows = CS50::query("SELECT * FROM places WHERE postal_code = ? OR
+                            MATCH (place_name, admin_name1) AGAINST (? IN BOOLEAN MODE)", $search_string, $search_string);
+    foreach ($rows as $row)
+    {
+        $places[] = [
+            "id" => $row["id"],
+            "country_code" => $row["country_code"],
+            "postal_code" => $row["postal_code"],
+            "place_name" => $row["place_name"],
+            "admin_name1" => $row["admin_name1"],
+            "admin_code1" => $row["admin_code1"],
+            "admin_name2" => $row["admin_name2"],
+            "admin_code2" => $row["admin_code2"],
+            "admin_name3" => $row["admin_name3"],
+            "admin_code3" => $row["admin_code3"],
+            "latitude" => $row["latitude"],
+            "longitude" => $row["longitude"],
+            "accuracy" => $row["accuracy"],
+        ];
+    }
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
